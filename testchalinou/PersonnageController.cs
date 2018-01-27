d7de48ef2e08ac03508f9fb3cc591b8e5e85c0f1
Happeal <metautbenjamin@hotmail.fr>
2018-01-27 02:46:19 +0100

    Add movement and special shoot

---
+++ testchalinou/PersonnageController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonnageController : MonoBehaviour {
+
+	private PersonnageModel model;
+
+	void Awake(){
+		model = this.gameObject.GetComponent<PersonnageModel> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		model.sprite = this.gameObject.GetComponent<SpriteRenderer> ();
+		model.shootPoint = transform.FindChild ("ShootPoint");
+
+		model.power = this.gameObject.GetComponent<Power> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float translation = Time.deltaTime * model.moveSpeed;
+
+		if (Input.GetKey (KeyCode.Space)) {
+			this.throwPower ();
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (new Vector3 (-translation, 0, 0));
+			//model.sprite.flipX = true;
+			Vector3 newScale = transform.localScale;
+			newScale.x *= -newScale.x;
+			transform.localScale = newScale;
+
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (new Vector3 (translation, 0, 0));
+
+			//model.sprite.flipX = false;
+			Vector3 newScale = transform.localScale;
+			newScale.x *= newScale.x;
+			transform.localScale = newScale;
+
+
+		} 
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (new Vector3 (0, translation, 0));
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (new Vector3 (0, -translation, 0));
+		}
+
+		
+	}
+
+	void throwPower(){
+		if (model.power != null) {
+			model.power.direction = (int) transform.localScale.x;
+			Instantiate (model.power, model.shootPoint.position, model.shootPoint.rotation);
+			model.power = null;
+		}
+	}
+}
---
+++ testchalinou/PersonnageModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonnageModel : MonoBehaviour {
+
+	public float percentage;
+	public float attackSpeed;
+	public float moveSpeed;
+	public int life;
+	public bool hasBonus;
+	public bool hasMalus;
+	public bool isShooting;
+	public Power power;
+	public bool isMoving;
+	public SpriteRenderer sprite;
+	public Transform shootPoint;
+
+
+}
