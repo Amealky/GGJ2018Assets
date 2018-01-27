ee0669c72d43bf940f70a26fc2d56496839a231c
Zakaria Aboukhriss <zak@weddingplanner.co.uk>
2018-01-27 19:17:38 +0100

    Merge Dou Brazil

--- Bonus/BonusScript.cs
+++ Bonus/BonusScript.cs
@@ -18,7 +18,6 @@ public class BonusScript : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
     	if (other.gameObject.tag == "Player"){
     		Destroy(gameObject);
-
 	    }
     }
 }
